9722b5929ad7c884f5aa023f9f8953e15ef4a5d6
maksimyakush <maksimo-1988@yandex.ru>
2017-05-16 02:52:14 +0300

    youtube/fix:visualisation

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+    },
+    "extends": "airbnb",
+    "rules": {
+        "indent": 0,
+        "arrow-body-style": 1,
+    }
+ };
--- scripts/script.js
+++ scripts/script.js
@@ -1,259 +1,232 @@
 let search;
 let nextPageToken;
 let pageNumber = 0;
-let arr = [];
+let pageMaxNumber = 0;
 let pagesArray = [];
 let mousePosition;
-let input = document.createElement("input");
+const input = document.createElement('input');
 document.body.appendChild(input);
-let itemsBlock = document.createElement("div");
+const itemsBlock = document.createElement('div');
 document.body.appendChild(itemsBlock);
-itemsBlock.classList.add("items");
+itemsBlock.classList.add('items');
 itemsBlock.draggable = true;
-let btnBlock = document.createElement("div");
-btnBlock.classList = "btn-block";
+const btnBlock = document.createElement('div');
+btnBlock.classList = 'btn-block';
 document.body.appendChild(btnBlock);
-let prevBtn = document.createElement("button");
-prevBtn.textContent = "Previous Page";
-prevBtn.classList = "prev-btn";
-let nextBtn = document.createElement("button");
-nextBtn.classList = "next-btn";
-nextBtn.textContent = "Next Page";
-let round = document.createElement("div");
-round.classList.add("round");
-let tooltip = document.createElement("span");
-tooltip.classList.add("tooltip");
+const prevBtn = document.createElement('button');
+prevBtn.textContent = 'Previous Page';
+prevBtn.classList = 'prev-btn';
+const nextBtn = document.createElement('button');
+nextBtn.classList = 'next-btn';
+nextBtn.textContent = 'Next Page';
+const round = document.createElement('div');
+round.classList.add('round');
+const tooltip = document.createElement('span');
+tooltip.classList.add('tooltip');
 document.body.appendChild(tooltip);
-input.addEventListener("change", ()=>{
-    search = input.value;
-    getFetch();
-})
-function getFetch(x="CAQQAQ") {
-    let searchUrl = new URL("https://www.googleapis.com/youtube/v3/search")
-    params = {
-        part: "snippet, id",
-        key: "AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw",
+function getFetch() {
+    pagesArray = [];
+    pageNumber = 0;
+    pageMaxNumber = 0;
+    const searchUrl = new URL('https://www.googleapis.com/youtube/v3/search');
+    const params = {
+        part: 'snippet, id',
+        key: 'AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw',
         maxResults: 15,
         q: search,
-        pageToken: x,
-    }
-    Object.keys(params).forEach(key=>searchUrl.searchParams.append(key, params[key]))
-    fetch(searchUrl).then(res=>res.json()).then(res=>{
+    };
+    Object.keys(params).forEach(key => searchUrl.searchParams.append(key, params[key]));
+    fetch(searchUrl).then(res1 => res1.json()).then((res) => {
         nextPageToken = res.nextPageToken;
-        prevPageToken = res.prevPageToken || "CAQQAQ";
-        let searchIdArr = [];
-        let html = res.items.map((c)=>{
-            searchIdArr.push(c.id.videoId)
-        });
-        return new Promise(resNext=>resNext(res))
+        return new Promise(resNext => resNext(res));
     })
-    .then(res=>{
-        let searchIdArr = [];
-        res.items.map(item=>{
-            searchIdArr.push(item.id.videoId)
-        })
-        let videosUrl = new URL("https://www.googleapis.com/youtube/v3/videos")
-        params1 = {
-            part: "snippet, statistics",
-            key: "AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw",
-            id: searchIdArr.join(","),
-        }
-        Object.keys(params1).forEach(key=>videosUrl.searchParams.append(key, params1[key]))
-        fetch(videosUrl).then(searchIdArr=>searchIdArr.json()).then(res=>{
-            res.items.map((item)=>{
-                arr.push(item);
-            });
-            arr.forEach((item,i)=>pagesArray.push(arr.splice(0, 5)))
-            let html = pagesArray[0].map(item=>{
-                return `<div class="item">
-                    <div class="main-block__item">
-                        <a href="https://www.youtube.com/watch?v=${item.id}" target="blank">
-                        <img src="${item.snippet.thumbnails.medium.url}"></img></a>
-                        <h4><a href="https://www.youtube.com/watch?v=${item.id}">${item.snippet.title}</a></h4>
+    .then((res) => {
+        const searchIdArr = [];
+        res.items.map(item => searchIdArr.push(item.id.videoId));
+        const videosUrl = new URL('https://www.googleapis.com/youtube/v3/videos');
+        const params1 = {
+            part: 'snippet, statistics',
+            key: 'AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw',
+            id: searchIdArr.join(','),
+        };
+        Object.keys(params1).forEach(key => videosUrl.searchParams.append(key, params1[key]));
+        fetch(videosUrl).then(res1 => res1.json()).then((res2) => {
+            const arr = [];
+            res2.items.map(item => arr.push(item));
+            arr.forEach(() => pagesArray.push(arr.splice(0, 3)));
+            if (arr.length > 0) pagesArray.push(arr);
+            const html = pagesArray[0].map((item) => {
+                return `<div class='item'>
+                    <div class='main-block__item'>
+                        <a href='https://www.youtube.com/watch?v=${item.id}' target='blank'>
+                        <img src='${item.snippet.thumbnails.medium.url}'></img></a>
+                        <h4><a href='https://www.youtube.com/watch?v=${item.id}'>${item.snippet.title}</a></h4>
                     </div>
-                    <ul class="features">
-                        <li class="channel">${item.snippet.channelTitle}</h5>
-                        <li class="published">${item.snippet.publishedAt.slice(0, 10)}</h5>
-                        <li class="views">${Number(item.statistics.viewCount).toLocaleString()}</h5>
+                    <ul class='features'>
+                        <li class='channel'>${item.snippet.channelTitle}</h5>
+                        <li class='published'>${item.snippet.publishedAt.slice(0, 10)}</h5>
+                        <li class='views'>${Number(item.statistics.viewCount).toLocaleString()}</h5>
                     </ul>
-                </div>`
-            }).join("");
+                </div>`;
+            }).join('');
             itemsBlock.innerHTML = html;
             btnBlock.appendChild(prevBtn);
             btnBlock.appendChild(round);
             round.textContent = 1;
             btnBlock.appendChild(nextBtn);
-            if (pageNumber == 0) {
-                prevBtn.classList.remove("prev-btn");
+            if (pageNumber === 0) {
+                prevBtn.classList.remove('prev-btn');
                 prevBtn.disabled = true;
-                prevBtn.classList.add("btn-disabled");
+                prevBtn.classList.add('btn-disabled');
             } else {
-                if (!prevBtn.classList.contains("prev-btn"))
-                    prevBtn.classList.add("prev-btn");
+                if (!prevBtn.classList.contains('prev-btn')) {
+                    prevBtn.classList.add('prev-btn');
+                }
                 prevBtn.disabled = false;
-                if (prevBtn.classList.contains("btn-disabled"))
-                    prevBtn.classList.remove("btn-disabled");
+                if (prevBtn.classList.contains('btn-disabled')) {
+                    prevBtn.classList.remove('btn-disabled');
+                }
             }
-        })
-    })
+        });
+    });
 }
-function getFetchNext(x = pageNumber) {
-    let searchUrl = new URL("https://www.googleapis.com/youtube/v3/search")
-    params = {
-        part: "snippet, id",
-        key: "AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw",
+function getFetchNext() {
+    const searchUrl = new URL('https://www.googleapis.com/youtube/v3/search');
+    const params = {
+        part: 'snippet, id',
+        key: 'AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw',
         maxResults: 15,
         q: search,
         pageToken: nextPageToken,
-    }
-    Object.keys(params).forEach(key=>searchUrl.searchParams.append(key, params[key]))
+    };
+    Object.keys(params).forEach(key => searchUrl.searchParams.append(key, params[key]));
     fetch(searchUrl)
-    .then(res=>res.json())
-    .then(res=>{
+    .then(res => res.json())
+    .then((res) => {
         nextPageToken = res.nextPageToken;
-        prevPageToken = res.prevPageToken || "CAQQAQ";
-        let searchIdArr = [];
-        let html = res.items.map((item)=>{
-            searchIdArr.push(item.id.videoId)
-        });
-        return new Promise(resNext=>resNext(res))
+        return new Promise(resNext => resNext(res));
     })
-    .then(res=>{
-        let searchIdArr = [];
-        res.items.map(item=>{
-            searchIdArr.push(item.id.videoId)
-        })
-        let videosUrl = new URL("https://www.googleapis.com/youtube/v3/videos")
-        params1 = {
-            part: "snippet, statistics",
-            key: "AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw",
-            id: searchIdArr.join(","),
-        }
-        Object.keys(params1).forEach(key=>videosUrl.searchParams.append(key, params1[key]))
+    .then((res) => {
+        const searchIdArr = [];
+        res.items.map(item => searchIdArr.push(item.id.videoId));
+        const videosUrl = new URL('https://www.googleapis.com/youtube/v3/videos');
+        const params1 = {
+            part: 'snippet, statistics',
+            key: 'AIzaSyCCJIXC9eWHVKbPxdyocYR9uWK8041DeCw',
+            id: searchIdArr.join(','),
+        };
+        Object.keys(params1).forEach(key => videosUrl.searchParams.append(key, params1[key]));
         fetch(videosUrl)
-        .then(searchIdArr=>searchIdArr.json())
-        .then(item=>{
-            item.items.map((item)=>{
-                arr.push(item);
-            })
-            arr.forEach((a,i)=>pagesArray.push(arr.splice(0, 5)))
-            let html = pagesArray[x].map(item=>{
-                return `<div class="item">
-                    <div class="main-block__item">
-                        <a href="https://www.youtube.com/watch?v=${item.id}" target="blank">
-                        <img src="${item.snippet.thumbnails.medium.url}"></img></a>
-                        <h4><a href="https://www.youtube.com/watch?v=${item.id}">${item.snippet.title}</a></h4>
-                    </div>
-                    <ul class="features">
-                        <li class="channel">${item.snippet.channelTitle}</h5>
-                        <li class="published">${item.snippet.publishedAt.slice(0, 10)}</h5>
-                        <li class="views">${Number(item.statistics.viewCount).toLocaleString()}</h5>
-                    </ul>
-                </div>`
-            }).join("");
-            itemsBlock.innerHTML = html;
-            btnBlock.appendChild(prevBtn);
-            btnBlock.appendChild(round);
-            round.textContent = pageNumber + 1;
-            btnBlock.appendChild(nextBtn);
-            if (pageNumber == 0) {
-                prevBtn.classList.remove("prev-btn");
-                prevBtn.disabled = true;
-                prevBtn.classList.add("btn-disabled");
-            } else {
-                if (!prevBtn.classList.contains("prev-btn"))
-                    prevBtn.classList.add("prev-btn");
-                prevBtn.disabled = false;
-                if (prevBtn.classList.contains("btn-disabled"))
-                    prevBtn.classList.remove("btn-disabled");
-            }
-        })
-    })
+        .then(res1 => res1.json())
+        .then((item) => {
+            const arr = [];
+            item.items.map(item1 => arr.push(item1));
+            arr.forEach(() => pagesArray.push(arr.splice(0, 3)));
+            if (arr.length > 0) pagesArray.push(arr);
+        });
+    });
 }
-function renderPrevPage(x=pageNumber) {
-    let html = pagesArray[x].map(item=>{
-        return `<div class="item">
-                    <div class="main-block__item">
-                        <a href="https://www.youtube.com/watch?v=${item.id}" target="blank">
-                        <img src="${item.snippet.thumbnails.medium.url}"></img></a>
-                        <h4><a href="https://www.youtube.com/watch?v=${item.id}">${item.snippet.title}</a></h4>
-                    </div>
-                    <ul class="features">
-                        <li class="channel">${item.snippet.channelTitle}</h5>
-                        <li class="published">${item.snippet.publishedAt.slice(0, 10)}</h5>
-                        <li class="views">${Number(item.statistics.viewCount).toLocaleString()}</h5>
-                    </ul>
-                </div>`
-    }).join("");
+function renderPage(x) {
+    const html = pagesArray[x].map((item) => {
+        return `<div class='item'>
+            <div class='main-block__item'>
+                <a href='https://www.youtube.com/watch?v=${item.id}' target='blank'>
+                <img src='${item.snippet.thumbnails.medium.url}'></img></a>
+                <h4><a href='https://www.youtube.com/watch?v=${item.id}'>${item.snippet.title}</a></h4>
+            </div>
+            <ul class='features'>
+                <li class='channel'>${item.snippet.channelTitle}</h5>
+                <li class='published'>${item.snippet.publishedAt.slice(0, 10)}</h5>
+                <li class='views'>${Number(item.statistics.viewCount).toLocaleString()}</h5>
+            </ul>
+        </div>`;
+    }).join('');
     itemsBlock.innerHTML = html;
     btnBlock.appendChild(prevBtn);
     btnBlock.appendChild(round);
     round.textContent = pageNumber + 1;
     btnBlock.appendChild(nextBtn);
-    if (pageNumber == 0) {
-        prevBtn.classList.remove("prev-btn");
+    if (pageNumber === 0) {
+        prevBtn.classList.remove('prev-btn');
         prevBtn.disabled = true;
-        prevBtn.classList.add("btn-disabled");
+        prevBtn.classList.add('btn-disabled');
     } else {
-        if (!prevBtn.classList.contains("prev-btn"))
-            prevBtn.classList.add("prev-btn");
+        if (!prevBtn.classList.contains('prev-btn')) {
+            prevBtn.classList.add('prev-btn');
+        }
         prevBtn.disabled = false;
-        if (prevBtn.classList.contains("btn-disabled"))
-            prevBtn.classList.remove("btn-disabled");
+        if (prevBtn.classList.contains('btn-disabled')) {
+            prevBtn.classList.remove('btn-disabled');
+        }
     }
 }
+
+// Control block
+input.addEventListener('change', () => {
+    search = input.value;
+    itemsBlock.innerHTML = '';
+    getFetch();
+});
 function handleDragStart(e) {
     mousePosition = e.clientX;
-
 }
 function handleDragEnd(e) {
     if (e.clientX < mousePosition - 70) {
-        pageNumber++;
-        getFetchNext()
+        pageNumber += 1;
+        if (pageMaxNumber < pageNumber) {
+            pageMaxNumber = pageNumber;
+            getFetchNext();
+        }
+        renderPage(pageNumber);
     } else if (e.clientX > mousePosition + 70 && pageNumber > 0) {
-        pageNumber--;
-        renderPrevPage();
+        pageNumber -= 1;
+        renderPage(pageNumber);
     }
 }
-nextBtn.addEventListener("mousedown", (e)=>{
-    tooltip.style.display = "inline";
-    let x = e.clientX
-    let y = e.clientY;
-    tooltip.style.top = (y - 40) + 'px';
-    tooltip.style.left = x + 'px';
-    tooltip.textContent = pageNumber + 2;
-}, false);
-prevBtn.addEventListener("mousedown", (e)=>{
-    tooltip.style.display = "inline";
-    let x = e.clientX
-    let y = e.clientY;
-    tooltip.style.top = (y - 40) + 'px';
-    tooltip.style.left = x + 'px';
-    if(pageNumber>0) tooltip.textContent = pageNumber - 1;
-}, false);
-nextBtn.addEventListener("mouseup", ()=>{
-    tooltip.style.display = "";
-});
-prevBtn.addEventListener("mouseup", ()=>{
-    tooltip.style.display = "";
-});
-nextBtn.addEventListener("mousemove", ()=>{
-    tooltip.style.display = "";
-});
-prevBtn.addEventListener("mousemove", ()=>{
-    tooltip.style.display = "";
-});
-
-itemsBlock.addEventListener("mousemove", ()=>{
-    tooltip.style.display = "";
-});
 itemsBlock.addEventListener('dragstart', handleDragStart, false);
 itemsBlock.addEventListener('dragend', handleDragEnd, false);
-nextBtn.addEventListener("click", ()=>{
-    pageNumber++;
-    getFetchNext()
+nextBtn.addEventListener('click', () => {
+    pageNumber += 1;
+    if (pageMaxNumber < pageNumber) {
+        pageMaxNumber = pageNumber;
+        getFetchNext();
+    }
+    renderPage(pageNumber);
 });
-prevBtn.addEventListener("click", ()=>{
-    pageNumber--;
-    renderPrevPage();
+prevBtn.addEventListener('click', () => {
+    pageNumber -= 1;
+    renderPage(pageNumber);
+});
+// End control block
+
+nextBtn.addEventListener('mousedown', (e) => {
+    tooltip.style.display = 'inline';
+    const x = e.clientX;
+    const y = e.clientY;
+    tooltip.style.top = `${y - 20}px`;
+    tooltip.style.left = `${x}px`;
+    tooltip.textContent = pageNumber + 2;
+}, false);
+prevBtn.addEventListener('mousedown', (e) => {
+    tooltip.style.display = 'inline';
+    const x = e.clientX;
+    const y = e.clientY;
+    tooltip.style.top = `${y - 20}px`;
+    tooltip.style.left = `${x}px`;
+    if (pageNumber > 0) tooltip.textContent = pageNumber;
+}, false);
+nextBtn.addEventListener('mouseup', () => {
+    tooltip.style.display = '';
+});
+prevBtn.addEventListener('mouseup', () => {
+    tooltip.style.display = '';
+});
+nextBtn.addEventListener('mousemove', () => {
+    tooltip.style.display = '';
+});
+prevBtn.addEventListener('mousemove', () => {
+    tooltip.style.display = '';
+});
+itemsBlock.addEventListener('mousemove', () => {
+    tooltip.style.display = '';
 });
